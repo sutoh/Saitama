87474890f79db3839de46fd5e07fa1a8c9eb5f2c
takuyamaeda <heavenlybigwednesday@gmail.com>
2013-01-24 22:20:46 +0900

    add profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,8 @@
 class ProfileController < ApplicationController
   def index
+  	@employee = Employee.find(current_user.employee_id)
+  	@employee_licenses = @employee.employee_licenses.all
+  	@employee_skills = @employee.employee_skills.all
+  	@work = @employee.works.all
   end
 end
