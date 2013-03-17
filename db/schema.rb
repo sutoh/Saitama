25cac4728005e5465b751c7ec3f6f453e78aa391
Yutaka Yawata <yuyawata@gmail.com>
2013-03-17 23:25:23 +0900

    generate "role" model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130317135645) do
+ActiveRecord::Schema.define(:version => 20130317140047) do
 
   create_table "customers", :force => true do |t|
     t.string   "name"
@@ -76,6 +76,15 @@ ActiveRecord::Schema.define(:version => 20130317135645) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
   end
+
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "roles", ["user_id"], :name => "index_roles_on_user_id"
 
   create_table "skills", :force => true do |t|
     t.string   "name"
