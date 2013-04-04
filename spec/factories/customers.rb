ce6f2e3641c7a40f541fecea52a9318eb6ffc27e
sutoh <sutoh.shohei@human-net.co.jp>
2013-04-04 14:36:55 +0900

    FactoriGirl追加 => customer.rb

---
+++ spec/factories/customers.rb
@@ -0,0 +1,10 @@
+# -*- encoding: UTF-8 ?*-
+FactoryGirl.define do
+  factory :customer do 
+     name "Human-net"
+     postalcode 3300855
+     address "Saitama"
+     website "http://www.google.com"
+     phone_num "090-1111-2222"
+  end
+end
