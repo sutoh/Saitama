a1514e5de2a73509889ba648db55e49dff1433f4
sutoh <sutoh.shohei@human-net.co.jp>
2013-04-04 17:17:07 +0900

    response.statusが200ではなく、302になってしまう問題の対処 対策 参考 : http://stackoverflow.com/questions/5865555/how-to-do-integration-testing-with-rspec-and-devise-cancan 原因 Failure/Error: response.status.should be(200) expected #<Fixnum:401> => 200 got #<Fixnum:605> => 302

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -3,7 +3,15 @@
     before(:each) do
     	controller.stub(:authenticate_user!).and_return true
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      sign_in FactoryGirl.create(:user)
+      @user  = FactoryGirl.create(:user)
+      sign_in @user
     end
+  end
+end
+
+module RequestMacros
+  def sign_in_user
+    @user = FactoryGirl.create(:user)
+    post_via_redirect user_session_path, 'user[login_id]' => @user.login_id, 'user[password]' => @user.password
   end
 end
--- spec/support/device.rb
+++ spec/support/device.rb
@@ -1,4 +1,5 @@
 ﻿RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
+  config.include RequestMacros, :type => :request
 end
