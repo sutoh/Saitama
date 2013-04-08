74b32cfd222b4bc4d003cb02e4249e3c6c074ea1
sutoh <sutoh.shohei@human-net.co.jp>
2013-04-08 14:15:30 +0900

    Webフォント設定

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Saitama
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Web font 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
   end
 end
