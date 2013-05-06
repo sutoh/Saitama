d0807759ebfcb67adb935e71e400244a645326a3
takuyamaeda <heavenlybigwednesday@gmail.com>
2013-05-06 18:24:54 +0900

    profile画面にて、単位を表示するように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   
   #権限管理用定数
   ADMIN = 1
-  $genders_list = [['Men', 0],['Women', 1]]
+  $genders_list = [['男性', 0],['女性', 1]]
   $tech_list = [['技術者', 1], ['営業', 2]]
   $staff_list = [['正社員', 1], ['契約社員', 2], ['個人事業主', 3], ['関連会社', 4]]
   
