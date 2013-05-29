84580b324fc5a245684b078eb4624beb347832c3
sutoh <sutoh.shohei@human-net.co.jp>
2013-05-29 18:03:29 +0900

    Model_Employee first とりあえず試しにMethods1つ

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,16 @@
+# -*- encoding: utf-8 -*-
 require 'spec_helper'
 
 describe Employee do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "gmaps4rails_address method" do
+    before do
+      employee = FactoryGirl.create(:employee_sutoh,address: 'Tokyo') 
+      employee.save
+    end
+    let(:employee){ Employee.where("address = ?",'Tokyo').first}
+    context "出力確認" do
+      it ("not null") { employee.gmaps4rails_address.should_not be_nil }
+      it ("正常出力") { employee.gmaps4rails_address.should == "Tokyo" }
+    end 
+  end
 end
