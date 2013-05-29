58fd61f281eda42966d01b6d40dd58ab7d432b2d
sutoh <sutoh.shohei@human-net.co.jp>
2013-05-29 15:31:32 +0900

    【spec】EmployeeRequest #32 使い方の勉強も兼ねて context,before,it(its),subject

--- spec/requests/employees_spec.rb
+++ spec/requests/employees_spec.rb
@@ -1,12 +1,31 @@
+# -*- encoding: UTF-8 -*-
 require 'spec_helper'
 
-describe "Employees" do
-  describe "GET /employees" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#テスト対象
+describe "EmployeeRequest" do
+  #状態・テスト条件(～した場合)
+  context "ログイン済みで/employeeをGETした場合" do
+    #テスト前に行う処理
+    before do 
       sign_in_user
+    end
+    before do
       get employees_path
-      response.status.should be(200)
+    end
+    
+    #テスト対象
+    subject { response }
+    #テスト
+    it '正常であること' do
+      status.should be(200)
+    end
+  end
+  
+  context "ログインせず、/employeeをGETした場合" do
+    before { get employees_path }
+    subject { response }
+    it 'ユーザログインページへ転送されること' do
+      status.should be(302)
     end
   end
 end
