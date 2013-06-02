e6815ed8096543bdac9304afc4e6e941b32a8431
takuyamaeda <heavenlybigwednesday@gmail.com>
2013-06-02 12:07:54 +0900

    #33 employeeテスト修正 ・なるべくshouldではなくexpectを使用するように変更 ・重複したテストはshare_examples_forで宣言 ・対象を表すときはdescribe、状況はcontextに書き方統一 ・一つのdescribeにまとめた

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,202 +1,184 @@
 # -*- encoding: utf-8 -*-
 require 'spec_helper'
 
-describe Employee,"with データ存在テスト:" do
+describe Employee do
+  share_examples_for 'Validation successfull' do
+    it ("エラーが発生しないこと") {expect{subject}.not_to raise_error}
+    it ("登録されること") { expect(subject).to be_true }
+  end
+  share_examples_for 'Column_is_not_nil_and_string' do
+    it ("Nullでない事") { expect(column).not_to be_nil }
+    it ("Stringである事") { expect(column).to be_an_instance_of(String) }
+  end
+  share_examples_for 'Column_is_not_nil_and_numeric' do
+    it ("Nullでない事") { expect(column).not_to be_nil }
+    it ("Numericである事") { expect(column).to be_a_kind_of(Numeric) }
+  end
 
   context "データが存在しない時" do
     subject { Employee.all }
     its (:size) { should eq 0 }
-    it ("Employeeが空である事") { should be_empty }
+    it ("データが空である事") { expect(subject).to be_empty }
   end
   
   context "データが存在する時" do
     subject { FactoryGirl.create_list(:employee, 3, :sutoh, :rubeus, :maeda) }
-    it ("Employeeが空でない事") { should_not be_empty }
+    it ("データが空でない事") { expect(subject).not_to be_empty }
     its (:size) { should eq 3 }
   end
-  
-end
-   
-describe Employee,"with カラムテスト:" do
 
-  subject { FactoryGirl.create(:employee_sutoh) }
-  
-  context "#family_name" do
-    let(:column){subject.family_name}
-    it { column.should eq 'sutoh' }
-    it ("Nullでない事") { column.should_not be_nil }
-    it ("Stringである事") { column.should be_an_instance_of(String) }
-  end
-  
-  context "#given_name" do
-    let(:column){subject.given_name}
-    it { column.should eq 'shohei' }
-    it ("Nullでない事") { column.should_not be_nil }
-    it ("Stringである事") { column.should be_an_instance_of(String) }
-  end
-  
-  context "#family_name_kana" do
-    let(:column){subject.family_name_kana}
-    it { column.should eq 'ストウ' }
-    it ("Nullでない事") { column.should_not be_nil }
-    it ("Stringである事") { column.should be_an_instance_of(String) }
-  end
-  
-  context "#given_name_kana" do
-    let(:column){subject.given_name_kana }
-    it { column.should eq 'ショウヘイ' }
-    it ("Nullでない事") { column.should_not be_nil }
-    it ("Stringである事") { column.should be_an_instance_of(String) }
-  end
-  
-  context "#gender" do
-    let(:column){subject.gender}
-    it ("男であること(1)"){ column.should eq 1 }
-    it ("Nullでない事") { column.should_not be_nil }
-    it ("Numericである事") { column.should be_a_kind_of(Numeric) }
-  end
-end
-
-describe Employee, "with Validation(Setting)Test" do
-  context "presence Check" do
-    it { should validate_presence_of(:family_name) }
-    it { should validate_presence_of(:given_name) }
-    it { should validate_presence_of(:address) }
-  end
-  
-  context "numericality Check" do
-    it { should validate_numericality_of(:gender) }
-    it { should validate_numericality_of(:experience) }
-    it { should validate_numericality_of(:tech_flg) }
-    it { should validate_numericality_of(:staff_flg) }
-  end
-end
-describe Employee, "with Validation(Maximum and minimum)Test" do
-
-  subject { FactoryGirl.create(:employee_sutoh,hash) }
-  
-  describe "最大文字数で登録した場合" do
+  describe "with カラムテスト:" do
+    subject { FactoryGirl.create(:employee_sutoh) }
     context "#family_name" do
-      let(:hash){ {family_name: 'sutoh1234567890sutoh'} }
-      let(:len){ subject.family_name.length }
-      it ("20文字であること") { expect(len).to eq 20 }
+      let(:column){subject.family_name}
+      it { expect(column).to eq 'sutoh' }
+      it_should_behave_like 'Column_is_not_nil_and_string'
     end
-    
+  
     context "#given_name" do
+      let(:column){subject.given_name}
+      it { expect(column).to eq 'shohei' }
+      it_should_behave_like 'Column_is_not_nil_and_string'
     end
-    
-  end
   
-  describe "最大文字数超で登録した場合" do
-    context "#family_name" do
-      let(:hash){ {family_name: 'sutoh1234567890sutohX'} }
-      it "エラーであること:(21文字)" do 
-        expect{
-          subject
-        }.to raise_error
+    context "#family_name_kana" do
+      let(:column){subject.family_name_kana}
+      it { expect(column).to eq 'ストウ' }
+      it_should_behave_like 'Column_is_not_nil_and_string'
+    end
+  
+    context "#given_name_kana" do
+      let(:column){subject.given_name_kana }
+      it { expect(column).to eq 'ショウヘイ' }
+      it_should_behave_like 'Column_is_not_nil_and_string'
+    end
+  
+    context "#gender" do
+      let(:column){subject.gender}
+      it ("男であること(1)"){ expect(column).to eq 1 }
+      it_should_behave_like 'Column_is_not_nil_and_numeric'
+    end
+  end
+
+  describe "with Validation(Setting)Test" do
+    context "presence Check" do
+      it { should validate_presence_of(:family_name) }
+      it { should validate_presence_of(:given_name) }
+      it { should validate_presence_of(:address) }
+    end
+  
+    context "numericality Check" do
+      it { should validate_numericality_of(:gender) }
+      it { should validate_numericality_of(:experience) }
+      it { should validate_numericality_of(:tech_flg) }
+      it { should validate_numericality_of(:staff_flg) }
+    end
+  end
+
+  describe "with Validation(Maximum and minimum)Test" do
+    subject { FactoryGirl.create(:employee_sutoh,hash) }
+    context "最大文字数で登録した場合" do
+      describe "#family_name" do
+        let(:hash){ {family_name: 'sutoh1234567890sutoh'} }
+        let(:len){ subject.family_name.length }
+        it_should_behave_like 'Validation successfull'
+        it ("20文字であること") { expect(len).to eq 20 }
+      end
+    
+      describe "#given_name" do
       end
     end
-    
-    context "#given_name" do
-    end
-    
-  end
   
-  describe "最小文字数以内で登録した場合" do
-    context "#family_name" do
-      let(:hash){ {family_name: 's'} }
-      let(:len){ subject.family_name.length }
-      it ("1文字であること") { expect(len).to eq 1 }
-    end
+    context "最大文字数超で登録した場合" do
+      describe "#family_name" do
+        let(:hash){ {family_name: 'sutoh1234567890sutohX'} }
+        it ("エラーであること:(21文字)") { expect{subject}.to raise_error}
+      end
     
-    context "#given_name" do
-    end
-    
-  end
-  
-  describe "最小文字数超で登録した場合" do
-    context "#family_name" do
-      let(:hash){ {family_name: ''} }
-      it "エラーであること:(0文字)" do 
-        expect{
-          subject
-        }.to raise_error
+      describe "#given_name" do
       end
     end
-    
-    context "#given_name" do
-    end
-    
-  end
   
-  describe "nilの場合" do
-    context "#family_name" do
-      let(:hash){ {family_name: nil} }
-      it "エラーであること:presence" do 
-        expect{
-          subject
-        }.to raise_error
+    context "最小文字数以内で登録した場合" do
+      describe "#family_name" do
+        let(:hash){ {family_name: 's'} }
+        let(:len){ subject.family_name.length }
+        it_should_behave_like 'Validation successfull'
+        it ("1文字であること") { expect(len).to eq 1 }
+      end
+    
+      describe "#given_name" do
       end
     end
-    
-    context "#given_name" do
-    end
-    
-    context "#station" do
-      let(:hash){ {station: nil} }
-      let(:station){ subject.station }
-      it "登録されること" do 
-        expect(subject).to be_true
+  
+    context "最小文字数超で登録した場合" do
+      describe "#family_name" do
+        let(:hash){ {family_name: ''} }
+        it ("エラーであること:(0文字)") {expect{subject}.to raise_error}
       end
-      it ("nilであること") { expect(station).to be_nil }
-    end
-  end
-  
-end
-  
-describe Employee,"with RelationTest" do
-
-  context "have_manyの確認" do
-    it { should have_many(:licenses).through(:employee_licenses) }
-    it { should have_many(:employee_skills) }
-    it { should have_many(:skills).through(:employee_skills) }
-    it { should have_many(:works) }
-  end
-  
-  context "belong_toの確認" do
-    it { should belong_to(:department) }
-    it { should belong_to(:user) }
-  end
-  
-end
-  
-describe Employee,"with MethodsTest" do
-  #Methodテスト
-  describe "gmaps4rails_address method" do
-    context "Address へ Saitama を入れた場合" do
     
-      before { FactoryGirl.create :employee_sutoh }
+      describe "#given_name" do
+      end
+    end
+  
+    context "nilの場合" do
+      describe "#family_name" do
+        let(:hash){ {family_name: nil} }
+        it ("エラーであること:presence") {expect{subject}.to raise_error}
+      end
       
-      let(:employee){ Employee.where("address = ?",'Saitama').first}
-      it ("nullでない事") { employee.gmaps4rails_address.should_not be_nil }
-      it ("正常に出力される事") { employee.gmaps4rails_address.should == "Saitama" }
+      describe "#given_name" do
+      end
+    
+      describe "#station" do
+        let(:hash){ {station: nil} }
+        it_should_behave_like 'Validation successfull'
+        its (:station){ should be_nil }
+      end
+    end
+  end
+
+  describe "with RelationTest" do
+    context "have_manyの確認" do
+      it { should have_many(:licenses).through(:employee_licenses) }
+      it { should have_many(:employee_skills) }
+      it { should have_many(:skills).through(:employee_skills) }
+      it { should have_many(:works) }
+    end
+  
+    context "belong_toの確認" do
+      it { should belong_to(:department) }
+      it { should belong_to(:user) }
+    end
+  end
+  
+  describe "with MethodsTest" do
+    #Methodテスト
+    describe "gmaps4rails_address method" do
+      context "Address へ Saitama を入れた場合" do
+    
+        before { FactoryGirl.create :employee_sutoh }
+        subject{Employee.where("address = ?",'Saitama').first.gmaps4rails_address}
       
-    end 
-  end
+        it ("nullでない事") { expect(subject).not_to be_nil }
+        it ("正常に出力される事") { expect(subject).to eq "Saitama" }
+      
+      end 
+    end
     
-  describe "gmaps4rails_infowindow" do
-  end
+    describe "gmaps4rails_infowindow" do
+    end
   
-  describe "fullname" do
-  end
+    describe "fullname" do
+    end
   
-  describe "fullname_kana" do
-  end
+    describe "fullname_kana" do
+    end
   
-  describe "nenrei" do
-  end
+    describe "nenrei" do
+    end
   
-  describe "gender_judge" do
+    describe "gender_judge" do
+    end
   end
-    
 end
