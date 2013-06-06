1ea985cd0325339d56e634c28e9098681ea523cd
sutoh <sutoh.shohei@human-net.co.jp>
2013-06-06 17:44:13 +0900

    #32 書き方変更

--- spec/requests/employees_spec.rb
+++ spec/requests/employees_spec.rb
@@ -1,31 +1,24 @@
 # -*- encoding: UTF-8 -*-
 require 'spec_helper'
-
 #テスト対象
-describe "EmployeeRequest" do
-  #状態・テスト条件(～した場合)
-  context "ログイン済みで/employeeをGETした場合" do
-    #テスト前に行う処理
-    before do 
-      sign_in_user
-    end
-    before do
-      get employees_path
+describe "EmployeesRequest" do
+
+  describe "GET index" do
+    subject { response.status }
+    
+    context "ログイン済み" do
+      before { sign_in_user }
+      before(:each) { get employees_path }
+      it '正常であること' do
+        expect(subject).to eq(200)
+      end
     end
     
-    #テスト対象
-    subject { response }
-    #テスト
-    it '正常であること' do
-      status.should be(200)
-    end
-  end
-  
-  context "ログインせず、/employeeをGETした場合" do
-    before { get employees_path }
-    subject { response }
-    it 'ユーザログインページへ転送されること' do
-      status.should be(302)
+    context "ログインしない場合" do
+      before(:each) { get employees_path }
+      it 'ユーザログインページへ転送されること' do
+        expect(subject).to eq(302)
+      end
     end
   end
 end
